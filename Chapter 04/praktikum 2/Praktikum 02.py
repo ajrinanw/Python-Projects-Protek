a5068ee6c5b5cdb179aa18762bee4ec2edcca4dc
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-10-17 22:50:46 +0700

    Create Praktikum 02.py

---
+++ Chapter 04/praktikum 2/Praktikum 02.py
@@ -0,0 +1,54 @@
+x = 10
+print(type(x))
+y = 20
+print(type(y))
+print(type(x+y))
+
+i = 3
+f = 3.5
+
+print (type(i+i))
+print (type(i+f))
+print (type(f+i))
+print (type(f+f))
+
+print (type(i-i))
+print (type(i-f))
+print (type(f-i))
+print (type(f-f))
+
+print (type(i*i))
+print (type(i*f))
+print (type(f*i))
+print (type(f*f))
+
+print (type(i/i))
+print (type(i/f))
+print (type(f/i))
+print (type(f/f))
+
+print (type(i//i))
+print (type(i//f))
+print (type(f//i))
+print (type(f//f))
+
+print (type(i%i))
+print (type(i%f))
+print (type(f%i))
+print (type(f%f))
+
+print (type(i**i))
+print (type(i**f))
+print (type(f**i))
+print (type(f**f))
+
+
+a = 10
+p = y = x = z = a
+print(a)
+print(z)
+print(x)
+print(y)
+print(p)
+
+
