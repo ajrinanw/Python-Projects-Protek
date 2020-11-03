b32ef1059c71afd790ffb117edb74c47829800a1
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 12:08:19 +0700

    Create Prak. 02 No. 01.py

---
+++ Chapter 06/Praktikum 2/Prak. 02 No. 01.py
@@ -0,0 +1,9 @@
+#CHAP.06 PRAK.02 NOMOR 01
+
+def luasSegitiga(a,t):
+    luas = a*t/2
+    return luas
+
+alas = 10
+tinggi = 20
+print("Luas Segitiga dengan Alas ", alas, "dan tinggi", tinggi, "adalah", luasSegitiga (alas,tinggi))
