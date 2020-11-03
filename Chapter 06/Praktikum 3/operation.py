7e89f6b7248a11d1541744056e73181fce7fa7a2
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 12:10:43 +0700

    Create operation.py

---
+++ Chapter 06/Praktikum 3/operation.py
@@ -0,0 +1,14 @@
+#CHAP.06 PRAK.03 NOMOR 01
+
+def jumlah(a,b):
+    hasil = a+b
+    return hasil
+def kali(a,b):
+    hasil = a*b
+    return hasil
+def kurang(a,b):
+    hasil = a-b
+    return hasil
+def bagi(a,b):
+    hasil = a/b
+    return hasil
