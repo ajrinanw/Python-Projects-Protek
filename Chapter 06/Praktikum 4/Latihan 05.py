bd539b67538423054f7a8dc34f0feb90879cfc76
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 12:13:53 +0700

    Create Latihan 05.py

---
+++ Chapter 06/Praktikum 4/Latihan 05.py
@@ -0,0 +1,14 @@
+#CHAP.06 PRAK.O4 LATIHAN 05
+from statistik import*
+
+#***Nomor 5a***
+print("5, 10, 4, 9, 30, 16, 2, 11")
+print("rata-rata :"), average(5, 10, 4, 9, 30, 16, 2, 11)
+print("nilai max :"), maks(5, 10, 4, 9, 30, 16, 2, 11)
+print("nilai min :"), min(5, 10, 4, 9, 30, 16, 2, 11)
+
+#***Nomor 5b***
+print("81, 98, 12, 83, 45, 77, 69, 30, 56")
+print("rata-rata :"), average(81, 98, 12, 83, 45, 77, 69, 30, 56)
+print("nilai max :"), maks(81, 98, 12, 83, 45, 77, 69, 30, 56)
+print("nilai min :"), min(81, 98, 12, 83, 45, 77, 69, 30, 56)
