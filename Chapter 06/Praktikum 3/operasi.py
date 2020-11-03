22e232f4dde616ae25c72f3386dd9e6d4c889070
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 12:11:20 +0700

    Create operasi.py

---
+++ Chapter 06/Praktikum 3/operasi.py
@@ -0,0 +1,10 @@
+#CHAP.06 PRAK.03 NOMOR 03
+
+from operation import*
+
+a=10
+b=7
+print (a, "+", b, "=", jumlah(a,b))
+print (a, "-", b, "=", kurang(a,b))
+print (a, "*", b, "=", kali(a,b))
+print (a, "/", b, "=", bagi(a,b))
