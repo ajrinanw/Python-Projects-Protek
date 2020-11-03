e09b598d1b40793d057cb4f299ae37651479c958
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 11:04:41 +0700

    Create Praktikum 01.py

---
+++ Chapter 05/Praktikum 1/Praktikum 01.py
@@ -0,0 +1,78 @@
+#CHAPTER 05 PRAKTIKUM 1
+#***nomor 2***
+bil = 10
+if (bil > 0):
+    print ("Bilangan Positif")
+
+#***nomor 4***
+bil = -4
+if (bil > 0):
+    print ("Bilangan Positif")
+else :
+    print ("Bilangan Negatif atau Nol")
+
+#***nomor 6***
+bil = 0
+if (bil > 0):
+    print ("Bilangan Positif")
+elif (bil < 0):
+    print ("Bilangan Negatif")
+else :
+    print ("Bilangan Nol")
+
+#***nomor 8***
+a = -2
+b = -7
+if (a > 0) and (b > 0):
+    print ("Keduanya Positif")
+elif (a > 0) or (b > 0):
+    print ("Salah Satunya Positif")
+elif (a < 0) or (b < 0):
+    print ("Salah Satunya Negatif")
+elif (a < 0) and (b < 0):
+    print ("Keduanya Negatif")
+
+#***nomor 10***
+a = -2
+b = -7
+if (a > 0) and (b > 0):
+    print ("Keduanya Positif")
+elif (a > 0) or (b > 0):
+    print ("Salah Satunya Positif")
+elif (a < 0) and (b < 0):
+    print ("Keduanya Negatif")
+elif (a < 0) or (b < 0):
+    print ("Salah Satunya Negatif")
+
+#***nomor 11***
+a = -2
+b = -7
+if (a > 0) and (b > 0):
+    print ("Keduanya Positif")
+if (a > 0) or (b > 0):
+    print ("Salah Satunya Positif")
+if (a < 0) or (b < 0):
+    print ("Salah Satunya Negatif")
+if (a < 0) and (b < 0):
+    print ("Keduanya Negatif")
+
+#***nomor 12a***
+a = 8
+b = 3
+if (a > 0) and (b > 0):
+    print ("Keduanya Positif")
+else :
+    print ("Keduanya Tidak Positif")
+
+#***nomor 12b***
+a = 8
+b = 3
+if (a > 0):
+    if (b > 0):
+        print ("Keduanya Positif")
+    else :
+        print ("Keduanya Tidak Positif")
+else :
+    print ("Keduanya Tidak Positif")
+    
+
