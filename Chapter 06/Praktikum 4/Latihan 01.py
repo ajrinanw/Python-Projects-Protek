a149fa2edd8410dacd69b0ff6a83724a1043c791
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 12:12:50 +0700

    Create Latihan 01.py

---
+++ Chapter 06/Praktikum 4/Latihan 01.py
@@ -0,0 +1,21 @@
+#CHAP.06 PRAK.04 LATIHAN 01
+
+def isPythagoras(a,b,c):
+    if ((a*a)==(c*c)-(b*b)and(b*b)==(c*c)-(a*a)and(c*c)==(a*a)+(b*b)):
+        print (True)
+
+    else:
+        print (False)
+
+#***Nomor 1a***
+isPythagoras (3,4,5)
+
+#***Nomor 1b***
+isPythagoras (5,9,12)
+
+#***Nomor 1c***
+isPythagoras (8,6,10)
+
+#***Nomor 1d***
+isPythagoras (7,8,11)
+
