d0acca4def335431c4232d16b5a9a9ab1d09cd85
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 11:09:04 +0700

    Create Latihan 03.py

---
+++ Chapter 05/Praktikum 2/Latihan 03.py
@@ -0,0 +1,14 @@
+#CHAP.05 PRAK.02 LATIHAN 03
+
+jmlh = 0
+i = 0
+angka = 1
+while (angka < 100):
+    print (angka)
+    jmlh += angka
+    angka += 2
+    i += 1
+
+print ("Banyaknya Bilangan Ganjil :", str(i))
+print ("Jumlah Seluruh Bilangan   :", str(jmlh))
+
