c91b8c1c1fe16e2b57cc547b2ec6f9c5189bace6
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-11-03 12:12:39 +0700

    Create Praktikum 04.py

---
+++ Chapter 06/Praktikum 4/Praktikum 04.py
@@ -0,0 +1,32 @@
+#CHAPTER 06 PRAKTIKUM 04
+'''
+#***Nomor 1***
+def jumlah(a,b):
+    hasil = a+b
+
+a=10
+b=20
+jumlah(a,b)
+print(hasil)
+'''
+#***Nomor 4***
+def jumlah(a,b):
+    global hasil
+    hasil = a+b
+a=10
+b=20
+jumlah(a,b)
+print(hasil)
+
+#***Nomor 5***
+def myFunction():
+    a=20
+    print (a)
+
+a=10
+myFunction()
+print (a)
+
+
+
+
