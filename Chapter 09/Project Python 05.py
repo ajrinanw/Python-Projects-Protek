6a3a6a064ffb3c49626199baa38f2d845dee98d4
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-12-22 05:24:52 +0700

    Create Project Python 05.py

---
+++ Chapter 09/Project Python 05.py
@@ -0,0 +1,15 @@
+#PHYTON PROJECT 05
+
+nilai = [{'nim' : 'A01', 'nama' : 'Agustina', 'mid' : 50, 'uas' : 80},
+         {'nim' : 'A02', 'nama' : 'Budi', 'mid' : 40, 'uas' : 90}, 
+         {'nim' : 'A03', 'nama' : 'Chicha', 'mid' : 100, 'uas' : 50}, 
+         {'nim' : 'A04', 'nama' : 'Donna', 'mid' : 20, 'uas' : 100},
+         {'nim' : 'A05', 'nama' : 'Fatimah', 'mid' : 70, 'uas' : 100}]
+
+print("=" * 43)
+print("NIM".ljust(10), "NAMA".ljust(10), "N.MID".rjust(10), "N.UAS".rjust(10))
+print("-" * 43)
+
+for i in  nilai :
+    print(i['nim'].ljust(10), i['nama'].upper().ljust(10), str(i['mid']).rjust(10), str(i['uas']).rjust(10))
+print("-" * 43)
