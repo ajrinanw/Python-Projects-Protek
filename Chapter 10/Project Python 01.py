acbcca9d05e400356a44b9c11c1c9ed78ba2a91d
ajrinasywa <ajrinanw@student.uns.ac.id>
2020-12-31 19:52:31 +0700

    Create Project Python 01.py

---
+++ Chapter 10/Project Python 01.py
@@ -0,0 +1,17 @@
+#PHYTON PROJECT 01
+
+file = open('fileAngka.txt','r')
+line = file.readlines()
+
+genap = []
+ganjil = []
+
+for i in range(len(line)) :
+    if(int(line[i])%2 == 0) :
+        genap.append(line[i])
+    else :
+        ganjil.append(line[i])
+
+print("Banyaknya bilangan genap  : {0}".format(len(genap)))
+print("Banyaknya bilangan ganjil : {0}".format(len(ganjil)))
+file.close()
